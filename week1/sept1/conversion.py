b84284df69514b2be3c9c1c0c289c02452af760b
sreejareddy-24 <23b81a66h5@cvr.ac.in>
2025-09-08 13:50:41 +0530

    uploaded week1 work

---
+++ week1/sept1/conversion.py
@@ -0,0 +1,6 @@
+#Write program to convert the distance (in feet) to inches, yards, and miles. 
+
+n=int(input("Enter the distance in feet"))
+print("Distance in inches:",n*12)
+print("Distance in yards:",n*(1/3))
+print("Distance in miles:",n*(1/5280))
